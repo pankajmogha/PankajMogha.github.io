42fe319939207522187976c11cedad63502a7db3
pankajmogha <66131840+pankajmogha@users.noreply.github.com>
2026-06-23 21:11:49 +0000

    Deploying to gh-pages from @ pankajmogha/PankajMogha.github.io@d51518f48b65bb9d54313d51bb0a0ca84348e642 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,19 +451,19 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-rna-structure-determination",
+          title: 'RNA structure determination',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-organ-on-chip",
+          title: 'Organ-on-chip',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-single-cell-spatial-transcriptomics",
+          title: 'Single-cell spatial transcriptomics',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
